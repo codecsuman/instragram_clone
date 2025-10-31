52b746a0a8bf33c80c1d242efb62be473e8ad214
SUMAN JHANP <sumanjhanp1@gmail.com>
2025-10-31 21:39:16 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -54,7 +54,7 @@ function App() {
 
   useEffect(() => {
     if (user) {
-      const socketio = io('http://localhost:8000', {
+      const socketio = io('https://instragram-clone-5.onrender.com', {
         query: { userId: user?._id },
         transports: ['websocket'],
       });
